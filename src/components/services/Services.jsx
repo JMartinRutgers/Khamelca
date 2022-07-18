67547a566d096e3528696c205c2ee2d1e6cad219
Jorge Melchor <48293166+JMartinRutgers@users.noreply.github.com>
2022-07-18 09:40:39 -0400

    Add files via upload

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -12,89 +12,112 @@ import Ho from '../../assets/Ho.png';
 
 
 const Services = () => {
-    return (    
-        <section  id = "services" > 
-          <h1 className = "alber" > K H A M E L C A </h1>     
-            
-           <h2>OUR SERVICES </h2>
-       <center> 
-       <h3>We offert the best prices in the area. 
-       Any time of season.
-    
-       </h3>
-       
-       </center>
-        <div className = "contenido" >
-           <article className = "cardleft" >
-           <div className = "card-container1 card-01" >
-           <div className = "pricing-card1 card-01" >
-           <div className = "pricing1" >
-           <div className = "price1" >
-           <sup > $ </sup>  
-           <span > 90 </span>  
-           </div>  
-           <p> /month</p>
-   
-           <span className = "type1" > Basic </span>  
-           </div>  
-           <div className = "card-body1" >
-   
-           <div className = "top-shape1" > </div>  
-           <div className = "card-content1" >
-   
-           <h5> Weekly lawn Mowing </h5>   
-           <h5> Lawn Repair </h5>   
-           <h5> Sedding </h5>  
-           <h5> Fertilization </h5> 
-   
-   
-           <a href = "#contact" className= "buy-btn1" > Buy Now </a>  
-           </div>  
-           </div>  
-           </div> 
-           </div>
-           </article>
+    return ( <
+        section id = "services" >
+        <
+        h1 className = "alber" > K H A M E L C A < /h1>     
 
-          <article>
-          <div class="pol">
-          <img src = { Ho } alt = "About_img" / >
-          <div className="services-titulo">
-          <h3>Landscaping</h3>
-          </div>
-          </div>
-          </article>
+        <
+        h2 > OUR SERVICES < /h2> <
+        center >
+        <
+        h3 > We offert the best prices in the area.Any time of season.
 
-          <article className = "cardright" >
-           <div className = "card-container2 card-02" >
-           <div className = "pricing-card2 card-02" >
-           <div className = "pricing2" >
-           <div className = "price2" >
-           <sup > $ </sup>  
-           <span > 120 </span>  
-           </div>  
-           <p> /month</p>
-   
-           <span className = "type2" > Premium </span>  
-           </div>  
-           <div className = "card-body2" >
-   
-           <div className = "top-shape2" > </div>  
-           <div className = "card-content2" >
-   
-           <h5> Weekly lawn Mowing </h5>   
-           <h5> Lawn Repair </h5>   
-           <h5> Sedding </h5>  
-           <h5> Fertilization </h5> 
-   
-   
-           <a href = "#contact" className= "buy-btn2" > Buy Now </a>  
-           </div>  
-           </div>  
-           </div> 
-           </div>
-           </article>
-          </div>
-        </section>
+        <
+        /h3>
+
+        <
+        /center> <
+        div className = "contenido" >
+        <
+        article className = "cardleft" >
+        <
+        div className = "card-container1 card-01" >
+        <
+        div className = "pricing-card1 card-01" >
+        <
+        div className = "pricing1" >
+        <
+        div className = "price1" >
+        <
+        sup > $ < /sup>   <
+        span > 90 < /span>   <
+        /div>   <
+        p > /month</p >
+
+        <
+        span className = "type1" > Basic < /span>   <
+        /div>   <
+        div className = "card-body1" >
+
+        <
+        div className = "top-shape1" > < /div>   <
+        div className = "card-content1" >
+
+        <
+        h5 > Weekly lawn Mowing < /h5>    <
+        h5 > Lawn Repair < /h5>    <
+        h5 > Sedding < /h5>   <
+        h5 > Fertilization < /h5> 
+
+
+        <
+        a href = "#contact"
+        className = "buy-btn1" > Buy Now < /a>   <
+        /div>   <
+        /div>   <
+        /div>  <
+        /div> <
+        /article>
+
+        <
+        article >
+
+        <
+        /article>
+
+        <
+        article className = "cardright" >
+        <
+        div className = "card-container2 card-02" >
+        <
+        div className = "pricing-card2 card-02" >
+        <
+        div className = "pricing2" >
+        <
+        div className = "price2" >
+        <
+        sup > $ < /sup>   <
+        span > 120 < /span>   <
+        /div>   <
+        p > /month</p >
+
+        <
+        span className = "type2" > Premium < /span>   <
+        /div>   <
+        div className = "card-body2" >
+
+        <
+        div className = "top-shape2" > < /div>   <
+        div className = "card-content2" >
+
+        <
+        h5 > Weekly lawn Mowing < /h5>    <
+        h5 > Lawn Repair < /h5>    <
+        h5 > Sedding < /h5>   <
+        h5 > Fertilization < /h5> 
+
+
+        <
+        a href = "#contact"
+        className = "buy-btn2" > Buy Now < /a>   <
+        /div>   <
+        /div>   <
+        /div>  <
+        /div> <
+        /article> <
+        /div> <
+        /section>
     )
 }
 
